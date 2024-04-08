9e90b391d9e11f9a64feccfe460fe54f2bb8baa5
tejasksaindane <tejassaindane99@gmail.com>
2024-04-08 23:10:18 +0530

    Navbar Authentication and Loading Page completed done

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -9,14 +9,22 @@ const font = Poppins({
 
 export const Logo = () => {
     return (
-        <div className="hidden md:flex items-center gap-x-2">
-            <Image
-                src="/logo.svg"
-                height="40"
-                width="40"
-                alt="Logo"
-            />
-            <p className={cn("font-semibold",font.className)}>Notion</p>
-        </div>
-    )
+      <div className="hidden md:flex items-center gap-x-2">
+        <Image
+          src="/logo.svg"
+          height="40"
+          width="40"
+          alt="Logo"
+          className="dark:hidden"
+        />
+        <Image
+          src="/logo-dark.svg"
+          height="40"
+          width="40"
+          alt="logo"
+          className="hidden dark:block"
+        />
+        <p className={cn("font-semibold", font.className)}>Notion</p>
+      </div>
+    );
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://many-marten-21.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
