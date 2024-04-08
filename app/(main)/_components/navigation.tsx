741509952f48207f3031b2fa6db2b67420bbf00e
tejasksaindane <tejassaindane99@gmail.com>
2024-04-09 00:55:59 +0530

    sidebar in progress

---
+++ app/(main)/_components/navigation.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { ChevronsLeftIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { ElementRef, useRef, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
+
+export const Navigation = () => {
+  const pathname = usePathname();
+  const isMobile = useMediaQuery("(max-width:768px)");
+
+  const isResizingRef = useRef(false);
+  const sidebarRef = useRef<ElementRef<"aside">>(null);
+  const navbarRef = useRef<ElementRef<"div">>(null);
+  const [isResetting, setIsResitting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
+  return (
+    <>
+      <aside
+        ref={sidebarRef}
+        className={cn(
+            "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
+            isResetting && "transition-all ease-in-out duration-300",
+            isMobile && "w-0"
+        )}
+      >
+        <div className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition">
+          <ChevronsLeftIcon className="h-6 w-6" />
+        </div>
+        <div>
+          <p>Action items</p>
+        </div>
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+        <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0 " />
+      </aside>
+    </>
+  );
+};
