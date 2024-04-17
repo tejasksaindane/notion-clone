4abc21bc06ece9e74415b9a2f9ce3c59c1b117e3
tejasksaindane <tejassaindane99@gmail.com>
2024-04-17 16:08:51 +0530

    Cover Image

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -7,23 +7,58 @@ import {
 } from "@/components/ui/dialog";
 
 import { useCoverImage } from "@/hooks/use-cover-image";
+import { SingleImageDropzone } from "../single-image-dropzone";
+import { useState } from "react";
+import { useEdgeStore } from "@/lib/edgestore";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
 
-export const CoverImageModal = () => { 
-    const coverImage = useCoverImage();
+export const CoverImageModal = () => {
+  const [file, setFile] = useState<File>();
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-    return (
-        <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
-            <DialogContent>
-                <DialogHeader>
-                    <h2 className="text-center text-lg font-semibold">
-                        Cover Image
-                    </h2>
-                </DialogHeader>
-                <div>
-                    TODO: Upload image
-                </div>
-            </DialogContent>
-        </Dialog>
-    )
+  const coverImage = useCoverImage();
+  const { edgestore } = useEdgeStore();
+  const update = useMutation(api.documents.update);
+  const params = useParams();
 
-}
+  const onClose = () => {
+    setFile(undefined);
+    setIsSubmitting(false);
+    setFile(file);
+  };
+
+  const onChange = async (file?: File) => {
+    if (file) {
+      setIsSubmitting(true);
+      setFile(file);
+
+      const res = await edgestore.publicFiles.upload({
+        file,
+      });
+      await update({
+        id: params.documentId as Id<"documents">,
+        coverImage: res.url,
+      });
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog open={coverImage.isOpen} onOpenChange={coverImage.onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <h2 className="text-center text-lg font-semibold">Cover Image</h2>
+        </DialogHeader>
+              <SingleImageDropzone
+                  className="w-full outline-none"
+                  disabled={isSubmitting}
+                  value={file}
+                  onChange={onChange}
+              />
+      </DialogContent>
+    </Dialog>
+  );
+};
