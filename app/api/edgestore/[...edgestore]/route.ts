ee97763b5f6494d19a904621029eed3c707ddd38
tejasksaindane <tejassaindane99@gmail.com>
2024-04-19 02:16:07 +0530

    Testing-03

--- app/api/edgestore/[...edgestore]/route.ts
+++ app/api/edgestore/[...edgestore]/route.ts
@@ -1,8 +1,8 @@
 import { initEdgeStore } from '@edgestore/server';
 import { createEdgeStoreNextHandler } from '@edgestore/server/adapters/next/app';
- 
+
 const es = initEdgeStore.create();
- 
+
 /**
  * This is the main router for the Edge Store buckets.
  */
@@ -12,13 +12,13 @@ const edgeStoreRouter = es.router({
             return true;
     })
 });
- 
+
 const handler = createEdgeStoreNextHandler({
   router: edgeStoreRouter,
 });
- 
+
 export { handler as GET, handler as POST };
- 
+
 /**
  * This type is used to create the type-safe client for the frontend.
  */
